06815ff6cf31d9bf29418dd4db08cccca4a7f93f
Rich Beilfuss <rdebeilfuss@gmail.com>
2022-05-31 12:08:54 -0500

    add files to repo

--- src/index.js
+++ src/index.js
@@ -1,17 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import fruits from './foods';
+import {choice, remove} from './helpers';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+let fruit = choice(fruits);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+console.log(`I would like one ${fruit}, please!`);
+console.log(`Here is your ${fruit}!`);
+console.log(`YUM! Can I please have another?`);
+
+let remaining = remove(fruit,fruits);
+
+console.log(`I am sorry, we are out of that fruit. We have ${remaining.length} left.`)
